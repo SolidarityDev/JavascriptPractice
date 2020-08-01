bf5178c98f7ed3305e130b44180593cde998cf91
Seraphin <aribi.rachid@hotmail.fr>
2020-08-01 22:55:29 +0200

    ex10.js

---
+++ ex10.js
@@ -0,0 +1,10 @@
+function say_my_name(my_string) {
+
+    my_string ="Heinsenberg"; 
+    if (my_string != 0)
+      console.log("My name is", my_string);
+    else
+      console.log("My name is toto");
+  }
+  say_my_name();
+  
